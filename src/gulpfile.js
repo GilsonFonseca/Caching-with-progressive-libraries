6d5bc055590ff458412f996c4ae9778ab0503389
GilsonFonseca <gilson@ice.ufjf.br>
2017-04-19 05:04:24 -0300

    Added Files

---
+++ src/gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var gulp = require('gulp');
+var path = require('path');
+var swPrecache = require('sw-precache');
+
+gulp.task('make-service-worker', function(callback) {
+  var rootDir = 'app';
+
+  swPrecache
+    .write(path.join(rootDir, 'serviceworker.js'), 
+	{
+		staticFileGlobs: [rootDir + '/**/*.{html,css,png,jpg,gif}',
+                        rootDir + '/js/*.js'],
+		stripPrefix: rootDir,
+		runtimeCaching: [
+		{
+        urlPattern: /https:\/\/www\.reddit\.com\/r\/\w{1,255}\.json/,
+                handler: 'networkFirst',
+                options: {
+                        cache: {
+                                name: 'titles'
+                        }
+                }
+		}],
+		importScripts: ['config.js','sync.js'],
+		navigateFallback: 'message.html',
+		
+	}, callback);
+
+});
